43d8bd6289a28c2c50b0136c04fc70a05aaf14a3
xuanxuan <463736788@qq.com>
2018-03-09 09:44:32 +0800

    上传一个DOME

---
+++ dome01/src/components/Header.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+      <el-container>
+        <el-header><span class="img_logo" @click="getData"></span></el-header>
+        <el-container>
+          <el-aside width="200px">
+            <v-folding :list='list'></v-folding>
+          </el-aside>
+          <el-main>
+            <v-news></v-news>
+          </el-main>
+        </el-container>
+      </el-container>
+  </div>
+</template>
+<script>
+import Folding from "./component/Folding.vue";
+import News from "./News.vue";
+// import Ajax from '../public/ajax.js'
+export default {
+  name: "Header",
+  data() {
+    return {
+      list: []
+    };
+  },
+  components: {
+    "v-folding": Folding,
+    "v-news": News
+  },
+  mounted() {
+    // this.getData();
+  },
+  methods: {
+    getData() {
+      this.$post("a").then(res => {
+        console.log(res);
+        this.list = res.data;
+      });
+    },
+  }
+};
+</script>
+<style scoped>
+.el-header span {
+  display: block;
+  width: 200px;
+  height: 60px;
+  /* background: url("../assets/logo.png") no-repeat; */
+}
+.el-header,
+.el-footer {
+  background-color: rgba(255, 0, 0, 0.534);
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
+
