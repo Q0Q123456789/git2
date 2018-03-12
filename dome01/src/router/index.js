878888d79bc3fb06e81b5d686b4d0a7af1899d01
xuanxuan <463736788@qq.com>
2018-03-12 11:08:59 +0800

    Revert "Revert "上传一个DOME""
    This reverts commit ee5dfbe533fb7e92d0aca7ca47dd95439b413b61.

---
+++ dome01/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import ElementUI from 'element-ui'
+import VueTouch from 'vue-touch'
+import Echarts from 'echarts'
+import ajax from '../public/ajax.js'
+import 'element-ui/lib/theme-chalk/index.css'
+import Header from '@/components/Header'
+import Home from '@/components/Home'
+import News from '@/components/News'
+
+Vue.prototype.$get = ajax.get
+Vue.prototype.$post = ajax.post
+Vue.prototype.$echarts = Echarts
+
+Vue.use(VueTouch, {
+  name: 'v-touch'
+})
+Vue.use(ElementUI)
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/',
+    name: 'Header',
+    component: Header
+  },
+  {
+    path: '/Home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/News',
+    name: 'News',
+    component: News
+  }
+  ]
+})
