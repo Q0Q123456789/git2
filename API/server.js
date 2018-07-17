7a117ce2999c62e213e5ac1ad037316dbf08cd1e
DESKTOP-U1J0KEJ\ROG <463736788@qq.com>
2018-07-17 10:04:25 +0800

    2018-07-17

--- API/server.js
+++ API/server.js
@@ -2,6 +2,16 @@ var express = require("express");
 var DB = require('./model/db.js');
 var ObjectId = require('mongodb').ObjectID;
 
+const crypto = require('crypto');
+const secret = 'abcdefg';
+
+const hash = crypto.createHmac('sha256', secret)
+    .update('qwe')
+    .digest('hex');
+console.log(hash);
+
+
+
 var app = express(); /*实例化使用*/
 var fs = require("fs");
 
@@ -14,99 +24,7 @@ app.all('*', function (req, res, next) {
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
-app.get('/performance/model/list', function (req, res) {
-    // fs.readFile( __dirname + "/" + "list.json", 'utf8', function (err, data) {
-    //     // console.log( data );
-    //     res.jsonp( data );
-    // });
-    DB.find("username", {}, function (err, data) {
-        DB.find("list", {}, function (err, list) {
-            if (err) {
-                var obj = {
-                    responseCode: "10008",
-                    responseMsg: err,
-                    data: null
-                }
-            } else {
-                var obj = {
-                    responseCode: "10001",
-                    responseMsg: "请求成功！",
-                    data: {
-                        data,
-                        list
-                    }
-                }
-            }
-            res.json(obj);
-        })
-    })
 
-})
-// app.post('/performance/model/list', function (req, res) {
-//   DB.find("username", {}, function (err, data) {
-//     DB.find("list", {}, function (err, list) {
-//       if (err) {
-//         var obj = {
-//           responseCode: "10008",
-//           responseMsg: err,
-//           data: null
-//         }
-//       } else {
-//         var obj = {
-//           responseCode: "10001",
-//           responseMsg: "请求成功！",
-//           responsedata: {
-//             data,
-//             list
-//           }
-//         }
-//       }
-//       res.json(obj);
-//     })
-//   })
-// })
-app.post('/performance/model/local', function (req, res) {
-    DB.find("startup_log", {}, function (err, data) {
-        // DB.find("list", {}, function (err, list) {
-        if (err) {
-            var obj = {
-                responseCode: "10008",
-                responseMsg: err,
-                data: null
-            }
-        } else {
-            var obj = {
-                responseCode: "10001",
-                responseMsg: "请求成功！",
-                responseMsg: "请求成功！",
-                data: {
-                    data
-                }
-            }
-        }
-        res.json(obj);
-        // })
-    })
-})
-app.post('/performance/model/list', function (req, res) {
-    DB.find("list", {}, function (err, data) {
-        // DB.find("list", {}, function (err, list) {
-        if (err) {
-            var obj = {
-                responseCode: "10008",
-                responseMsg: err,
-                data: null
-            }
-        } else {
-            var obj = {
-                responseCode: "10001",
-                responseMsg: "请求成功！",
-                data
-            }
-        }
-        res.json(obj);
-    })
-})
 
 app.listen(8081);
 console.log('Listening on port 8081...')
